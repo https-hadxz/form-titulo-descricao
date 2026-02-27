115b2010b130554eda21f2318e1ae8335df5cfcb
https-hadxz <hadassacintron@gmail.com>
2026-02-27 15:11:57 -0300

    atualizando

--- form-titulo-descricao/servidor.js
+++ form-titulo-descricao/servidor.js
@@ -30,7 +30,7 @@ app.get('/titulo-descricao', (request, response) => {
 app.post('/titulo-descricao', (request, response) => {
     let registro = { titulo: request.body.titulo, descricao: request.body.descricao, imagem: request.body.imagem }
     vetreg.push(registro)
-    fs.writeFileSync('dados/titulo-descricao.json', JSON.stringify(registro))
+    fs.writeFileSync('dados/titulo-descricao.json', JSON.stringify(vetreg))
     response.render('form', {registro: `O título e a descrição foram registrados!`})
 })
 
