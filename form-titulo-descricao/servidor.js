3984fe922afbde6c8d1e5a944fda28ffce07f541
https-hadxz <hadassacintron@gmail.com>
2026-02-27 14:55:33 -0300

    Vamo atualizar pessoal

--- form-titulo-descricao/servidor.js
+++ form-titulo-descricao/servidor.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
+let vetreg = JSON.parse(fs.readFileSync('dados/titulo-descricao.json', 'utf8'))
+
+app.get('/', (request, response) => {
+    response.render('index', {lista: vetreg})
+})
 
 app.get('/index', (request, response) => {
-    response.render('index')
+    response.redirect('/')
 })
 
 app.get('/contato', (request, response) => {
@@ -20,9 +26,9 @@ app.get('/titulo-descricao', (request, response) => {
     response.render('form')
 })
 
-let vetreg = []
+
 app.post('/titulo-descricao', (request, response) => {
-    let registro = { titulo: request.body.titulo, descricao: request.body.descricao }
+    let registro = { titulo: request.body.titulo, descricao: request.body.descricao, imagem: request.body.imagem }
     vetreg.push(registro)
     fs.writeFileSync('dados/titulo-descricao.json', JSON.stringify(registro))
     response.render('form', {registro: `O título e a descrição foram registrados!`})
